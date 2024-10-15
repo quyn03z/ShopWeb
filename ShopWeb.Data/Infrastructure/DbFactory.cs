9d0c6df17506e82ea395c18d2b4b14a870c7ee01
quyn03z <quyenvn03@gmail.com>
2024-10-16 00:29:17 +0700

    add entities freamework code first

---
+++ ShopWeb.Data/Infrastructure/DbFactory.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShopWeb.Data.Infrastructure
+{
+	public class DbFactory : Disposable, IDbFactory
+	{
+		ShopWebDbContext dbContext;
+		public ShopWebDbContext Init()
+		{
+			return dbContext ?? (dbContext = new ShopWebDbContext());
+		}
+		protected override void DisposeCore()
+		{
+			if(dbContext != null) dbContext.Dispose();
+		}
+	}
+}
---
+++ ShopWeb.Data/Infrastructure/UnitOfWork.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShopWeb.Data.Infrastructure
+{
+	public class UnitOfWork : IUnitOfWork
+	{
+		private readonly IDbFactory dbFactory;
+		private ShopWebDbContext dbContext;
+
+		public UnitOfWork(IDbFactory dbFactory)
+		{
+			this.dbFactory = dbFactory;
+		}
+
+		public ShopWebDbContext DbContext()
+		{
+			return dbContext ?? (dbContext = new ShopWebDbContext());
+		}
+
+		public void Commit()
+		{
+			dbContext.SaveChanges();
+		}
+	}
+}
